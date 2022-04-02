9f2d39b33d807b317583cba3ad55acc417c26ad6
gautambaghel <gautam@gautam-c02dn48gmd6r.attlocal.net>
2022-04-01 19:25:58 -0700

    edited azure vms

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
   }
 
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.27"
-    }
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
     }
   }
 
@@ -31,42 +31,52 @@ provider "aws" {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  subscription_id = "befd0888-7a90-428e-bec4-cd7750a6a82c"
 }
 
-# Azure -- Deploy Linux VM
-resource "azurerm_linux_virtual_machine" "az_linux_vm" {
-  name                = "az_linux_vm-machine"
-  resource_group_name = "virtual-machines"
-  location            = "West US 2"
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
+resource "azurerm_resource_group" "example" {
+  name     = "virtual-machines"
+  location = "West US 2"
+}
 
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
 # Azure -- Deploy Windows VM
 resource "azurerm_windows_virtual_machine" "az_windows_vm" {
-  name                = "az_windows_vm-machine"
+  computer_name       = "az-win-vm"
+  name                = "az-win-vm"
   resource_group_name = "virtual-machines"
   location            = "West US 2"
-  size                = "Standard_F2"
+  size                = "Standard_D2ads_v5"
   admin_username      = "adminuser"
   admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
 
   os_disk {
     caching              = "ReadWrite"
